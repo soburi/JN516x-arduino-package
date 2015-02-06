bcfe889a4ca88732a6092f843eaf71fa0b8ba8bf
soburi <soburi@gmail.com>
2015-02-07 06:48:18 +0900

    basic compilation environment

---
+++ hardware/arduino/jen/cores/arduino/main.cpp
@@ -0,0 +1,62 @@
+/*
+  main.cpp - Main loop for Arduino sketches
+  Copyright (c) 2005-2013 Arduino Team.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+#include <Arduino.h>
+extern "C" void vUTIL_UartText(char *pcString);
+
+//Declared weak in Arduino.h to allow user redefinitions.
+int atexit(void (*func)()) { return 0; }
+
+// Weak empty variant initialization function.
+// May be redefined by variant files.
+void initVariant() __attribute__((weak));
+void initVariant() { }
+
+int main(void)
+{
+	init();
+
+	initVariant();
+
+#if defined(USBCON)
+	USBDevice.attach();
+#endif
+	
+	setup();
+    
+	for (;;) {
+		vUTIL_UartText("--- enter loop ---\r\n");
+		loop();
+		vUTIL_UartText("--- exit  loop ---\r\n");
+//		if (serialEventRun) serialEventRun();
+	}
+        
+	return 0;
+}
+
+extern "C"  void AppColdStart(void)
+{
+	main();
+}
+
+extern "C" void AppWarmStart(void)
+{
+	AppColdStart();
+}
---
+++ hardware/arduino/jen/cores/arduino/wiring.c
@@ -0,0 +1,158 @@
+/*
+  wiring.c - Partial implementation of the Wiring API for the ATmega8.
+  Part of Arduino - http://www.arduino.cc/
+
+  Copyright (c) 2005-2006 David A. Mellis
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General
+  Public License along with this library; if not, write to the
+  Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+  Boston, MA  02111-1307  USA
+
+  $Id$
+*/
+
+#include "wiring_private.h"
+
+#include <AppHardwareApi.h>
+void vUTIL_UartText(char *pcString);
+// the prescaler is set so that timer0 ticks every 64 clock cycles, and the
+// the overflow handler is called every 256 ticks.
+#define MICROSECONDS_PER_TIMER0_OVERFLOW (clockCyclesToMicroseconds(32))
+
+// the whole number of milliseconds per timer0 overflow
+#define MILLIS_INC (MICROSECONDS_PER_TIMER0_OVERFLOW / 1000)
+
+// the fractional number of milliseconds per timer0 overflow. we shift right
+// by three to fit these numbers into a byte. (for the clock speeds we care
+// about - 8 and 16 MHz - this doesn't lose precision.)
+#define FRACT_INC ((MICROSECONDS_PER_TIMER0_OVERFLOW % 1000) >> 3)
+#define FRACT_MAX (1000 >> 3)
+
+volatile unsigned long timer0_overflow_count = 0;
+volatile unsigned long timer0_millis = 0;
+static unsigned char timer0_fract = 0;
+
+#define TICKS_TO_USEC (32)
+#define TICK_TIMER_MAX (0x0fffffff)
+
+static void ticktimer_callback(uint32 u32Device, uint32 u32ItemBitmap)
+{
+	// copy these to local variables so they can be stored in registers
+	// (volatile variables must be read from memory on every access)
+	unsigned long m = timer0_millis;
+	unsigned char f = timer0_fract;
+
+	m += MILLIS_INC;
+	f += FRACT_INC;
+	if (f >= FRACT_MAX) {
+		f -= FRACT_MAX;
+		m += 1;
+	}
+
+	timer0_fract = f;
+	timer0_millis = m;
+	timer0_overflow_count++;
+}
+
+unsigned long millis()
+{
+	unsigned long m;
+	m = timer0_millis;
+	return m;
+}
+
+unsigned long micros() {
+	unsigned long m;
+	uint32_t t;
+
+	m = timer0_overflow_count;
+
+	t = u32AHI_TickTimerRead();
+	
+	return (t/16);
+}
+
+void delay(unsigned long ms)
+{
+	uint32_t start = (uint32_t)micros();
+
+	while (ms > 0) {
+		yield();
+		if (((uint32_t)micros() - start) >= 1000) {
+			ms--;
+			start += 1000;
+		}
+	}
+}
+
+/* Delay for the given number of microseconds.  Assumes a 8 or 16 MHz clock. */
+void delayMicroseconds(unsigned int us)
+{
+}
+
+void init()
+{
+	u32AHI_Init();
+
+	//UART
+	vAHI_UartEnable(E_AHI_UART_0);
+
+	vAHI_UartReset(E_AHI_UART_0, TRUE, TRUE);
+	vAHI_UartReset(E_AHI_UART_0, FALSE, FALSE);
+
+	vAHI_UartSetClockDivisor(E_AHI_UART_0, E_AHI_UART_RATE_115200);
+	vAHI_UartSetControl(E_AHI_UART_0, FALSE, FALSE, E_AHI_UART_WORD_LEN_8, TRUE, FALSE);
+
+	//Tick Timer
+	vAHI_TickTimerConfigure(E_AHI_TICK_TIMER_DISABLE);
+	vAHI_TickTimerWrite(0);
+	vAHI_TickTimerInterval(TICK_TIMER_MAX);
+	vAHI_TickTimerIntEnable(true);
+
+	vAHI_TickTimerRegisterCallback(ticktimer_callback);
+	vAHI_TickTimerConfigure(E_AHI_TICK_TIMER_CONT);
+}
+
+
+
+/****************************************************************************
+ *
+ * NAME: vUTIL_UartText
+ *
+ * DESCRIPTION:
+ * Sends text string to the UART
+ *
+ * PARAMETERS:      Name            RW  Usage
+ *                  pcString        R   Pointer to zero-terminated string
+ *
+ ****************************************************************************/
+PUBLIC void vUTIL_UartText(char *pcString)
+{
+    while (*pcString)
+    {
+        /* Wait for TX FIFO empty */
+        while ((u8AHI_UartReadLineStatus(E_AHI_UART_0) & E_AHI_UART_LS_THRE) == 0);
+
+        /* Send the character */
+        vAHI_UartWriteData(E_AHI_UART_0, *pcString);
+        if (*pcString == '\n')
+        {
+            vAHI_UartWriteData(E_AHI_UART_0, '\r');
+        }
+
+        /* Move to next character */
+        pcString++;
+    }
+}
+
