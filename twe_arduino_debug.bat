032b5ffa70b495008e38e15f460e8c3e1b17888c
soburi <soburi@gmail.com>
2015-02-10 01:57:40 +0900

    remove files and remove linker ref.

--- twe_arduino_debug.bat
+++
@@ -1,2 +0,0 @@
-set PATH=C:\TWESDK\Tools\ba-elf-ba2\bin;C:\TWESDK\Tools\ba-elf-ba2\ba-elf\bin;C:\TWESDK\Tools\ba-elf-ba2\libexec\gcc\ba-elf\4.1.2;C:\TWESDK\Tools\cygwin\bin;%PATH%
-arduino_debug.exe
