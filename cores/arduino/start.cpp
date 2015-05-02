d381a40afd03b357640d0d9709ec828bda32a3f0
soburi <soburi@gmail.com>
2015-05-02 21:37:35 +0900

    Call global variable initializer on ColdBoot.

--- cores/arduino/start.cpp
+++ cores/arduino/start.cpp
@@ -1,7 +1,20 @@
+#include <stdint.h>
+
+#include "Arduino.h"
+
 extern int main(void);
+
+extern "C" {
+	typedef void (*funcptr)(); 
+	extern funcptr ctors_start;
+}
 
 extern "C"  void AppColdStart(void)
 {
+	// call global initializers.
+	funcptr fp = ctors_start;
+	fp();
+
 	main();
 }
 
