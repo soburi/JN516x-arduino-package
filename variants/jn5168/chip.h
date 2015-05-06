690c2299a5bf351e70114d3969c99678685aa4cf
soburi <soburi@gmail.com>
2015-05-07 00:47:31 +0900

    Changes the enable/disable irq in the definition of MicroSpecific.h.

--- variants/jn5168/chip.h
+++ variants/jn5168/chip.h
@@ -1,2 +1,20 @@
+#ifndef _VARIANT_CHIP_H_
+#define _VARIANT_CHIP_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <jendefs.h>
+#include <MicroSpecific.h>
+
 #define SystemCoreClock F_INTERNAL_SYSCLOCK
+
+#define __disable_irq() MICRO_DISABLE_INTERRUPTS()
+#define __enable_irq()  MICRO_ENABLE_INTERRUPTS()
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //_VARIANT_CHIP_H_
