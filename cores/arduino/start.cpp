2c48dc53e0cba4713358c983bbf68a34b7a92f70
soburi <soburi@gmail.com>
2015-05-10 20:56:57 +0900

    Call all global initializer in .ctors section, to terminate when reached to the .end_ctors section.

--- cores/arduino/start.cpp
+++ cores/arduino/start.cpp
@@ -12,9 +12,13 @@ extern "C" {
 extern "C"  void AppColdStart(void)
 {
 	// call global initializers.
-	funcptr fp = ctors_start;
-	fp();
+	unsigned long* ptr = reinterpret_cast<unsigned long*>(&ctors_start);
 
+	// Terminate with .end_ctors section header.
+	while(*ptr) {
+		funcptr fp = reinterpret_cast<funcptr>(*ptr++);
+		fp();
+	}
 	main();
 }
 
