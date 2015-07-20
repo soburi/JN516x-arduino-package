86411405f28c4f91591168815f6fd45d53c7e4ae
soburi <soburi@gmail.com>
2015-07-20 22:23:25 +0900

    Insert spin loop for stabilize UART at warm boot.

--- cores/arduino/start.cpp
+++ cores/arduino/start.cpp
@@ -2,6 +2,15 @@
 
 #include "Arduino.h"
 
+#include <dbg.h>
+
+#ifndef DBG_UART
+#define DBG_UART DBG_E_UART_0
+#endif
+#ifndef DBG_BAUD_RATE
+#define DBG_BAUD_RATE DBG_E_UART_BAUD_RATE_9600
+#endif
+
 extern int main(void);
 
 extern "C" {
@@ -12,15 +21,9 @@ extern "C" {
 extern "C"  void AppColdStart(void)
 {
 #ifdef DBG_ENABLE
-
-#ifndef DBG_UART
-#define DBG_UART DBG_E_UART_0
-#endif
-#ifndef DBG_BAUD_RATE
-#define DBG_BAUD_RATE DBG_E_UART_BAUD_RATE_9600
-#endif
 	DBG_vUartInit(DBG_UART, DBG_BAUD_RATE);
 #endif
+	DBG_PRINTF("AppColdStart\r\n");
 	// call global initializers.
 	unsigned long* ptr = reinterpret_cast<unsigned long*>(&ctors_start);
 
@@ -34,7 +37,12 @@ extern "C"  void AppColdStart(void)
 
 extern "C" void AppWarmStart(void)
 {
-	AppColdStart();
+#ifdef DBG_ENABLE
+	DBG_vUartInit(DBG_UART, DBG_BAUD_RATE);
+	for(int i=0; i<1000; i++) ; //stabilize uart.
+#endif
+	DBG_PRINTF("AppWarmStart\r\n");
+	main();
 }
 
 
