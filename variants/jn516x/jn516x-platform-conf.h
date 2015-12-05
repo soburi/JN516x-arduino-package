67d8befaf8bf5b0643bdae9e2c622145bf128026
soburi <soburi@gmail.com>
2015-12-05 09:32:19 +0900

    Adding contiki sources and binaries, Support arduino 1.6.7

---
+++ variants/jn516x/jn516x-platform-conf.h
@@ -0,0 +1,11 @@
+#ifndef JN516X_PLATFORM_CONF_H
+#define JN516X_PLATFORM_CONF_H
+
+#define NETSTACK_CONF_MAC     csma_driver
+#define NETSTACK_CONF_RDC     contikimac_driver
+
+#undef PLATFORM_CONF_H
+#include "platform-conf.h"
+
+#endif /*JN516X_PLATFORM_CONF_H*/
+
