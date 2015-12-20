8cca35f17dd729ee0de9a4dae99aad5e5a710828
soburi <soburi@gmail.com>
2015-12-21 07:25:20 +0900

    Support stm32nucleo-spirit1

---
+++ variants/stm32nucleo-spirit1/platform.h
@@ -0,0 +1,19 @@
+#ifndef _VARIANT_PLATFORM_H
+#define _VARIANT_PLATFORM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#undef F_CPU
+
+#include <contiki.h>
+#include <net/netstack.h>
+#include <dev/watchdog.h>
+#include <lib/random.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //PLATFORM_DEPEND_INCLUDES_H
