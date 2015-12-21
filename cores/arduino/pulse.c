4858e8180ef205043fe0b4c56ea057f931c6b058
soburi <soburi@gmail.com>
2015-12-21 21:42:15 +0900

    Implements init and pulseIn as stub for concrete implementation in variants folder

--- cores/arduino/pulse.c
+++ cores/arduino/pulse.c
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2015 Arduino LLC.  All right reserved.
+  Copyright (c) TOKITA Hiroshi.  All right reserved.
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
@@ -16,43 +16,11 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#include <Arduino.h>
+#include <watchdog.h>
 
-// See pulse_asm.S
-extern unsigned long countPulseASM(const volatile uint32_t *port, uint32_t bit, uint32_t stateMask, unsigned long maxloops);
-
-/* Measures the length (in microseconds) of a pulse on the pin; state is HIGH
- * or LOW, the type of pulse to measure.  Works on pulses from 2-3 microseconds
- * to 3 minutes in length, but must be called at least a few dozen microseconds
- * before the start of the pulse. */
-uint32_t pulseIn(uint32_t pin, uint32_t state, uint32_t timeout)
+uint32_t _pulseInDefault(uint32_t pin, uint32_t state, uint32_t timeout)
 {
-	(void)pin;
-	(void)state;
-	(void)timeout;
-#if 0 //TODO
-	// cache the port and bit of the pin in order to speed up the
-	// pulse width measuring loop and achieve finer resolution.  calling
-	// digitalRead() instead yields much coarser resolution.
-	PinDescription p = g_APinDescription[pin];
-	uint32_t bit = p.ulPin;
-	uint32_t stateMask = state ? bit : 0;
-	
-	// convert the timeout from microseconds to a number of times through
-	// the initial loop; it takes (roughly) 18 clock cycles per iteration.
-	uint32_t maxloops = microsecondsToClockCycles(timeout) / 18;
-
-	uint32_t width = countPulseASM(&(p.pPort->PIO_PDSR), bit, stateMask, maxloops);
-
-	// convert the reading to microseconds. The loop has been determined
-	// to be 18 clock cycles long and have about 16 clocks between the edge
-	// and the start of the loop. There will be some error introduced by
-	// the interrupt handlers.
-	if (width)
-		return clockCyclesToMicroseconds(width * 18 + 16);
-	else
-		return 0;
-#endif
 	return 0;
 }
 
+uint32_t pulseIn(uint32_t pin, uint32_t state, uint32_t timeout) __attribute__ ((weak, alias("_pulseInDefault")) );
