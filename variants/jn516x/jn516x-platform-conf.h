1e01b0dcf2d030ea60e8c97adca762040cf6d1fd
soburi <soburi@gmail.com>
2016-02-17 22:53:20 +0900

    Catch up to upstream

--- variants/jn516x/jn516x-platform-conf.h
+++ variants/jn516x/jn516x-platform-conf.h
@@ -1,9 +1,6 @@
 #ifndef JN516X_PLATFORM_CONF_H
 #define JN516X_PLATFORM_CONF_H
 
-#define NETSTACK_CONF_MAC     csma_driver
-#define NETSTACK_CONF_RDC     contikimac_driver
-
 #undef PLATFORM_CONF_H
 #include "platform-conf.h"
 
