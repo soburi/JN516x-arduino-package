b4a56a3e18efdc22a6018d4a1334b76e79dbea9b
soburi <soburi@gmail.com>
2016-05-19 23:04:38 +0900

    Apply contiki-jn516x-main.c diffs

--- variants/jn516x/contiki-jn516x-main.c
+++ variants/jn516x/contiki-jn516x-main.c
@@ -230,7 +230,7 @@ start_uip6(void)
   if(!UIP_CONF_IPV6_RPL) {
     uip_ipaddr_t ipaddr;
     int i;
-    uip_ip6addr(&ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
+    uip_ip6addr(&ipaddr, UIP_DS6_DEFAULT_PREFIX, 0, 0, 0, 0, 0, 0, 0);
     uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
     uip_ds6_addr_add(&ipaddr, 0, ADDR_TENTATIVE);
     PRINTF("Tentative global IPv6 address ");
@@ -418,7 +418,6 @@ initVariant( void )
 #endif /* NETSTACK_CONF_WITH_IPV4 */
 
   watchdog_start();
-  NETSTACK_LLSEC.init();
 
 #if NETSTACK_CONF_WITH_IPV6
   start_uip6();
