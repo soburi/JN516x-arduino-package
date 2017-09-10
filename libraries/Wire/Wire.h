038457fe5a5f615c73ecfa21b8b82f7a4aee9df4
soburi <soburi@gmail.com>
2017-09-10 19:06:57 +0900

    Rename variant/jn516x/platform_wire[.c|.h] to libraries/Wire/Wire[.c|.h].
    class Wire no longer has platform specific implements.
    Move it into Wire[.c|.h] to commonly usable implementation.

--- libraries/Wire/Wire.h
+++ libraries/Wire/Wire.h
@@ -1,32 +1,107 @@
 /*
-  Copyright (c) TOKITA Hiroshi.  All right reserved.
+ * TWI/I2C library for Arduino Zero
+ * Copyright (c) 2015 Arduino LLC. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
+#ifndef TwoWire_h
+#define TwoWire_h
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-  See the GNU Lesser General Public License for more details.
+#include "Stream.h"
+#include "variant.h"
 
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+#include "wiring_private.h"
+#include "RingBuffer.h"
 
-#ifndef CONTIKIWIRE_H
-#define CONTIKIWIRE_H
+ // WIRE_HAS_END means Wire has end()
+#define WIRE_HAS_END 1
 
-#include "platform.h"
+class TwoWire : public Stream
+{
+  public:
+    TwoWire(struct i2c_device *i2cdev);
+    void begin();
+    void begin(uint8_t);
+    void end();
+    void setClock(uint32_t);
 
-#ifdef   PLATFORM_WIRE_HEADER
-#include PLATFORM_WIRE_HEADER
-#else
+    void beginTransmission(uint8_t);
+    uint8_t endTransmission(bool stopBit);
+    uint8_t endTransmission(void);
 
-/* TODO implement */
+    uint8_t requestFrom(uint8_t address, size_t quantity, bool stopBit);
+    uint8_t requestFrom(uint8_t address, size_t quantity);
 
-#endif // PLATFORM_WIRE_HEADER
+    size_t write(uint8_t data);
+    size_t write(const uint8_t * data, size_t quantity);
 
-#endif // CONTIKIWIRE_H
+    virtual int available(void);
+    virtual int read(void);
+    virtual int peek(void);
+    virtual void flush(void);
+    void onReceive(void(*)(int));
+    void onRequest(void(*)(void));
+
+    inline size_t write(unsigned long n) { return write((uint8_t)n); }
+    inline size_t write(long n) { return write((uint8_t)n); }
+    inline size_t write(unsigned int n) { return write((uint8_t)n); }
+    inline size_t write(int n) { return write((uint8_t)n); }
+    using Print::write;
+
+    void onService(void);
+
+  private:
+    struct i2c_device* i2c;
+    uint32_t clock;
+    bool master_mode;
+
+    bool transmissionBegun;
+
+    // RX Buffer
+    RingBuffer rxBuffer;
+
+    //TX buffer
+    RingBuffer txBuffer;
+    uint8_t txAddress;
+
+    // Callback user functions
+    void (*onRequestCallback)(void);
+    void (*onReceiveCallback)(int);
+
+    // TWI clock frequency
+    static const uint32_t TWI_CLOCK = 100000;
+};
+
+#if WIRE_INTERFACES_COUNT > 0
+  extern TwoWire Wire;
+#endif
+#if WIRE_INTERFACES_COUNT > 1
+  extern TwoWire Wire1;
+#endif
+#if WIRE_INTERFACES_COUNT > 2
+  extern TwoWire Wire2;
+#endif
+#if WIRE_INTERFACES_COUNT > 3
+  extern TwoWire Wire3;
+#endif
+#if WIRE_INTERFACES_COUNT > 4
+  extern TwoWire Wire4;
+#endif
+#if WIRE_INTERFACES_COUNT > 5
+  extern TwoWire Wire5;
+#endif
+
+#endif
