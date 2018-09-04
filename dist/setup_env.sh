68f697058ed418cb57d0d0cff0ab849f5cbeee19
soburi <soburi@gmail.com>
2018-09-04 21:41:06 +0900

    Apply GitHub's recommends versioning rule

--- dist/setup_env.sh
+++ dist/setup_env.sh
@@ -10,16 +10,18 @@ if [ "${TRAVIS_TAG}" == "" ] ; then
   RELEASEVER=${TRAVIS_BRANCH}-0.0.$(date -d @`git log -1 ${TRAVIS_COMMIT} --pretty=medium --format=%ct` +%y%m%d%H%M)
   BM_FORCEOPT='-f'
 else
+  RELEASELINE=${GHREPO_NAME}
   RELEASELINE_=$(echo ${TRAVIS_TAG} | sed 's/-.*$//')
-  if [ "${RELEASELINE_}" != "${TRAVIS_TAG}" ] ; then
+  if [[ $TRAVIS_TAG =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]] ; then
+    RELEASELINE="intiki"
+  elif [ "${RELEASELINE_}" != "${TRAVIS_TAG}" ] ; then
     RELEASELINE=${RELEASELINE_}
-  else
-    RELEASELINE=${GHREPO_NAME}
   fi
+
   PKGJSON=package_${GHREPO_USER}_${RELEASELINE}_index.json
   ARCHIVENAME=${GHREPO_NAME}-${TRAVIS_TAG}
   ARCHIVEURL="https://github.com/${TRAVIS_REPO_SLUG}/releases/download/${TRAVIS_TAG}/${ARCHIVENAME}.tar.bz2"
-  RELEASEVER=${TRAVIS_TAG}
+  RELEASEVER=$(echo ${TRAVIS_TAG} | rev | sed 's/^\([0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\).*$/\1/' | rev )
   BM_FORCEOPT=
 fi
 
